69bfdd80110bb725a4bd1bcd8e8724dec632ab4e
jinyu.li <jinyu.li@ecovacs.com>
2019-10-25 09:17:12 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {DatePicker} from 'antd';
+import 'antd/dist/antd.css';
+
+
+ReactDOM.render(<DatePicker/>, document.getElementById('root'));
